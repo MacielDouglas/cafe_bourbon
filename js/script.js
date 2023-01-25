64d0ca5d6a616a71af7ebafd4d21fce00b8bafbd
MacielDouglas <douglasmaciel.dm@gmail.com>
2023-01-25 10:00:40 -0300

    adicionado escolha moagem assinatura

--- js/script.js
+++ js/script.js
@@ -1,18 +1,15 @@
-function update() {
-  var select = document.getElementById("inputMensal");
-  var value = select.options[select.selectedIndex].value;
-  var tipo_cafe = document.getElementById("moido_tipo");
-  console.log(value);
+function updates(valo) {
+  const selecionado = document.getElementById(valo).value;
 
-  if (value == "2") {
-    tipo_cafe.classList.remove("hide");
+  const tipo_cafe = document.getElementById('moido_' + valo);
+
+  if (valo === 'coffeLove' && selecionado === '2') {
+    tipo_cafe.classList.remove('hide');
+  } else if (valo === 'mensal' && selecionado === '2') {
+    tipo_cafe.classList.remove('hide');
+  } else if (valo === 'trimestral' && selecionado === '2') {
+    tipo_cafe.classList.remove('hide');
   } else {
-    tipo_cafe.classList.add("hide");
+    tipo_cafe.classList.add('hide');
   }
 }
-
-function update2(){
-    update2() = update();
-    
-} 
-
