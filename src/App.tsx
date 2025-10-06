9ff258c3cca68777b16d28008ae02766f3b48ea2
MacielDouglas <douglasmaciel.dm@gmail.com>
2025-10-06 09:56:33 -0300

    feat: initialize cafe_bourbon project with Vite, React, and Tailwind CSS
    - Added package.json with dependencies for React, Tailwind CSS, and TanStack Router.
    - Included Vite configuration for React and Tailwind CSS.
    - Created initial application structure with App component and routing setup.
    - Added SVG assets for Vite and React logos.
    - Implemented basic routing with a root route and an index route.
    - Configured TypeScript with strict settings and bundler mode.
    - Set up ESLint for code linting.

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({
+  routeTree: routeTree,
+  defaultPendingMs: 0,
+  defaultPreload: "intent",
+});
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve("./src"),
+    },
+  },
+  plugins: [
+    tailwindcss(),
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
+  ],
+});
